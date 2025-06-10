7a2c3f88dbdc225809e3426f5ece4ebeef2dd666
Davi Oliveira <davi.ol.santos@gmail.com>
2025-06-10 08:41:32 -0300

    criando projeto com tailwind

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="p-8 bg-slate-900 text-slate-100 h-screen">
+      <h1 className="font-bold text">Hello tailwind</h1>
+      <h2>Hello tailwind</h2>
+      <p>Hello tailwind</p>
+    </div>
+  );
+}

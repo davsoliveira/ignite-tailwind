c763e43356d9d1d3bdec6e3fda7f102a2d123a98
Davi Oliveira <davi.ol.santos@gmail.com>
2025-06-11 11:13:21 -0300

    criando perfil do usuário

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     extend: {
       gridTemplateColumns: {
         app: 'minmax(18rem, 20rem) 1fr',
+        profile: 'max-content 1fr min-content',
       },
 
       maxWidth: {
